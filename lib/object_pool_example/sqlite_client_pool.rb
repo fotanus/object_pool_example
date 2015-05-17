c980a81cb2c4b1c17319d232603a313882ba736b
fotanus@gmail.com <Felipe Tanus>
2015-05-17 18:42:39 -0300

    Change object array by a queue

--- lib/object_pool_example/sqlite_client_pool.rb
+++ lib/object_pool_example/sqlite_client_pool.rb
@@ -1,16 +1,18 @@
 require "singleton"
+require "thread"
 
 module ObjectPoolExample
   class SQLiteClientPool
     include Singleton
 
+    MAX_OBJECTS = 4
     def initialize
-      @objects = []
+      @objects = Queue.new
       @objects_created = 0
     end
 
     def acquire_sqlite_client
-      if @objects.empty?
+      if @objects.empty? and @objects_created < MAX_OBJECTS
         @objects_created += 1
         SQLiteClient.new
       else
--- spec/object_pool_example/sqlite_client_pool_spec.rb
+++ spec/object_pool_example/sqlite_client_pool_spec.rb
@@ -18,58 +18,64 @@ module ObjectPoolExample
       end
 
       it "Always recover the same pool instance" do
-        i1 = subject.instance
-        i2 = subject.instance
-        expect(i1).to be i2
+        expect(subject.instance).to be subject.instance
       end
     end
 
-    describe "#acquire_sqlite_client" do
+    describe "instance methods" do
       subject { SQLiteClientPool.instance }
 
-      describe "On first call" do
-        it "creates a sqlite client client" do
-          expect(subject.acquire_sqlite_client).to be_kind_of(SQLiteClient)
+      before(:each) do
+        pool = SQLiteClientPool.instance
+        pool.instance_variable_set(:@objects_created, 0)
+        pool.instance_variable_get(:@objects).clear
+      end
+
+      describe "#acquire_sqlite_client" do
+        describe "On first call" do
+          it "creates a sqlite client client" do
+            expect(subject.acquire_sqlite_client).to be_kind_of(SQLiteClient)
+          end
+
+          it "increments the object counter" do
+            expect{
+              subject.acquire_sqlite_client
+            }.to change {
+              subject.instance_variable_get(:@objects_created)
+            }.by(1)
+          end
         end
 
-        it "increments the object counter" do
+        describe "If has an object on the queue" do
+          let(:client) { subject.acquire_sqlite_client}
+          before(:each) { subject.release_sqlite_client(client) }
+
+          it "Gives that object" do
+            expect(subject.acquire_sqlite_client).to be client
+          end
+
+          it "Won't create a new client" do
+            expect{
+              subject.acquire_sqlite_client
+            }.to_not change {
+              subject.instance_variable_get(:@objects_created)
+            }
+          end
+        end
+
+        pending "queue blocks when can't create objects and no one is left"
+      end
+
+      describe "#release_sqlite_client" do
+        let(:client) { subject.acquire_sqlite_client }
+
+        it "Returns one sqlite client" do
           expect{
-            subject.acquire_sqlite_client
+            subject.release_sqlite_client(client)
           }.to change {
-            subject.instance_variable_get(:@objects_created)
+            subject.instance_variable_get(:@objects).size
           }.by(1)
         end
-      end
-
-      describe "If has an object on the queue" do
-        let(:client) { subject.acquire_sqlite_client}
-        before(:each) { subject.release_sqlite_client(client) }
-
-        it "Gives that object" do
-          expect(subject.acquire_sqlite_client).to be client
-        end
-
-        it "Won't create a new client" do
-          expect{
-            subject.acquire_sqlite_client
-          }.to_not change {
-            subject.instance_variable_get(:@objects_created)
-          }
-        end
-      end
-
-    end
-
-    describe "#release_sqlite_client" do
-      subject { SQLiteClientPool.instance }
-      let(:client) { subject.acquire_sqlite_client }
-
-      it "Returns one sqlite client" do
-        expect{
-          subject.release_sqlite_client(client)
-        }.to change {
-          subject.instance_variable_get(:@objects).count
-        }.by(1)
       end
     end
   end
